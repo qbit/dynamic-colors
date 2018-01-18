defacddfec313e9a5ed3a12b61089d9ca946f0bf
Aaron Bieber <aaron@bolddaemon.com>
2018-01-18 07:19:26 -0700

    e-ink + nord themes

---
+++ colorschemes/eink.sh
@@ -0,0 +1,25 @@
+# eink
+
+background="white"
+foreground="black"
+cursor="green"
+mouse_background=""
+mouse_foreground=""
+highlight="#d6d6d6"
+border="black"
+color0="black" # black
+color1="red" # red
+color2="green" # green
+color3="yellow" # yellow
+color4="blue" # blue
+color5="magenta" # magenta
+color6="cyan" # cyan
+color7="white" # white
+color8="brblack" # brblack
+color9="brred" # brred
+color10="brgreen" # brgreen
+color11="#fced16" # bryellow
+color12="brblue" # brblue
+color13="brmagenta" # brmagenta
+color14="brcyan" # brcyan
+color15="brwhite" # brwhite
